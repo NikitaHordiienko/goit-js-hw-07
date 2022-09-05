a31e4051d8a02bf8e2944ba1c26dcdfd2adb862e
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-05 19:16:34 +0300

    Готовое задание 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsListEl = galleryItems.map(el => {
+    const galleryItemEl = document.createElement('div');
+    galleryItemEl.classList.add('gallery__item');
+
+    const galleryLinkEl = document.createElement('a');
+    galleryLinkEl.classList.add('gallery__link');
+    galleryLinkEl.href = el.original;
+
+    const galleryImageEl = document.createElement('img');
+    galleryImageEl.classList.add('gallery__image');
+    galleryImageEl.src = el.preview;
+    galleryImageEl.setAttribute('data-source', el.original)
+    galleryImageEl.alt = el.description;
+
+    galleryItemEl.append(galleryLinkEl);
+    galleryLinkEl.append(galleryImageEl);
+    
+
+    return galleryItemEl;
+})
+
+galleryEl.append(...galleryItemsListEl);
+console.log(galleryEl);
+
+galleryEl.addEventListener('click', event => {
+    event.preventDefault();
+
+    const { target } = event;
+    
+    if (target.nodeName !== 'IMG') {
+        return;
+    };
+
+    const instance = basicLightbox.create(`<img src="${target.dataset.source}">`, {
+        onShow: () => window.addEventListener('keydown', OnEscBtnPress),
+        onClose: () => window.removeEventListener('keydown', OnEscBtnPress)
+    });
+
+    function OnEscBtnPress(event) {
+        console.log(event.code);
+        if (event.code === 'Escape') {
+            instance.close()
+        };
+    };
+
+    instance.show();  
+});
