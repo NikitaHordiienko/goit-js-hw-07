da7980e1c84f0c1df75d8ed8b672b102a0990267
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-05 20:06:37 +0300

    Готовое 2 задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsListEl = galleryItems.map(el => {
+    const galleryLinkEl = document.createElement('a');
+    galleryLinkEl.classList.add('gallery__link');
+    galleryLinkEl.href = el.original;
+
+    const galleryImageEl = document.createElement('img');
+    galleryImageEl.classList.add('gallery__image');
+    galleryImageEl.src = el.preview;
+    galleryImageEl.alt = el.description;
+
+    galleryLinkEl.append(galleryImageEl);    
+
+    return  galleryLinkEl;
+})
+
+galleryEl.append(...galleryItemsListEl);
+console.log(galleryEl);
+
+const lightBox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
